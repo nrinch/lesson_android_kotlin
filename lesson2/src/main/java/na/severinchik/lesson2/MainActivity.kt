a7b7460cfa47094c69b2b5569f4468520a662817
Nikita <anillnize@gmail.com>
2021-08-11 21:44:50 +0300

    add lesson 3

--- lesson2/src/main/java/na/severinchik/lesson2/MainActivity.kt
+++ lesson2/src/main/java/na/severinchik/lesson2/MainActivity.kt
@@ -1,9 +1,14 @@
 package na.severinchik.lesson2
 
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.lang.NullPointerException
 
 class MainActivity : AppCompatActivity() {
+
+    val appContext by lazy { Application().baseContext }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,10 +39,9 @@ val resultSum = var1?.times(2)
 
 val whenVariable: Int = 42
 
-val arrayList = arrayListOf<Int>(32,5,646,3)
+val arrayList = arrayListOf<Int>(32, 5, 646, 3)
 
 fun whenExample() {
-
 
 
     when (whenVariable) {
@@ -51,19 +55,166 @@ fun whenExample() {
 }
 
 
-val range:IntRange = var2..whenVariable
+val range: IntRange = var2..whenVariable
 
 
+open class Person(var name: String) {
+    constructor(name: String, age: Int) : this(name) {
 
-open class Person(var name:String){
-    constructor(name:String, age:Int):this(name)
-    {
-
-    val birthdayYear = 2021 - age
+        val birthdayYear = 2021 - age
     }
 }
 
-class SuperPerson(name:String,surname:String):Person(name){}
+class SuperPerson(name: String, surname: String) : Person(name) {}
+
+open class Rectangle() {
+    open fun draw() {
+        print("")
+    }
+
+    open val borderColor = ""
+}
+
+class FilledRectangle : Rectangle() {
+    val filler = Filler()
+
+    override fun draw() {
+        filler.drawAndFill()
+    }
+
+    override val borderColor: String get() = "black"
+
+
+    inner class Filler {
+        fun fill() { /* ... */
+        }
+
+        fun drawAndFill() {
+            super@FilledRectangle.draw()
+            fill()
+            println("Нарисованный прямоугольник заполнен ${super@FilledRectangle.borderColor} цветом.")
+        }
+    }
+}
+
+fun main() {
+    val filler = FilledRectangle().Filler()
+    filler.drawAndFill()
+}
+
+interface Shape {
+
+    fun drawBorder()
+    fun drawBody()
+    fun fill()
+
+    fun draw() {
+        drawBody()
+        drawBorder()
+        fill()
+    }
+
+    fun dontdothis() = Application().baseContext
+}
+
+
+abstract class Abstractia {
+    open val count = 3
+
+    abstract fun doMagic()
+
+    fun superMagic() {
+        //TODO
+        doMagic()
+    }
+}
+
+
+class User {
+
+    private var _isOnline = false
+
+    val isOnline
+        get() = false
+
+    var a = Int
+
+
+    var name: String = ""
+        get() = Application().baseContext.toString()
+        set(value) {
+            field = value
+        }
+
+    init {
+        name = ""
+
+
+    }
+}
+
+
+fun Int.returnNull(): Int? = null
+
+//fun main() {
+//    var counter: Int? = null
+//
+//    counter = 0
+//    counter++
+//    counter = counter.returnNull()
+//
+//    var result = (5).sqrt()
+//    result.sqrtResult
+//
+//
+//    val i = counter.ifNotNull()
+//
+//    var su = SuperUser("","")
+//    var su2 = su.copy(name = "1")
+//}
+
+private fun Int.sqrt(): Int = this * this
 
 
 
+fun Any?.ifNotNull(): Any {
+    if (this != null) {
+        return this!!
+    } else {
+        return throw NullPointerException()
+    }
+
+}
+
+val Int.sqrtResult: Int
+    get() = this * this
+
+val String.Companion.EMPTY
+    get() = ""
+
+val String.Companion.SPACE
+    get() = " "
+
+
+data class SuperUser(val name:String,val surname: String)
+
+sealed class APIResponseState{
+    object Loading: APIResponseState()
+    data class Loaded(val message: String):APIResponseState()
+    data class Error(val messageError: String):APIResponseState()
+}
+
+
+
+fun requestSuccess():APIResponseState = APIResponseState.Loaded("Johny")
+
+//fun main() {
+//   val result = requestSuccess()
+//
+//    when(result){
+//        is APIResponseState.Error -> println("Error ${result.messageError}")
+//        is APIResponseState.Loaded -> println("")
+//        APIResponseState.Loading -> TODO()
+//        A -> TODO()
+//    }
+//}
---
+++ lesson3/src/main/java/na/severinchik/lesson3/MainActivity.kt
@@ -0,0 +1,46 @@
+package na.severinchik.lesson3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    var operation: CalcOperation? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val equal: Button = findViewById(R.id.am_operation_equal)
+        equal.setOnClickListener {
+            findViewById<TextView>(R.id.am_output_tv).text = "Bu-ga-ga"
+        }
+    }
+
+    fun operationEqual(v: View) {
+        findViewById<TextView>(R.id.am_output_tv).text = "Bu-ga-ga"
+    }
+
+    fun calcOperation(v: View) {
+        val button = v as Button
+        when(button.text){
+            "+" -> operation = CalcOperation.Plus
+            "-" -> operation = CalcOperation.Minus
+            "/" -> operation = CalcOperation.Mult
+            "*" -> operation = CalcOperation.Div
+        }
+      val outputField =  findViewById<TextView>(R.id.am_output_tv)
+        outputField.text = "${outputField.text} ${operation?.type}"
+
+    }
+}
+
+sealed class CalcOperation(val type: String) {
+    object Plus : CalcOperation("+")
+    object Minus : CalcOperation("-")
+    object Mult : CalcOperation("*")
+    object Div : CalcOperation("/")
+}
